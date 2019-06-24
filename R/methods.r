5e843e1e77c49973ea2a638457a9d5858e88c901
aghaynes <aghaynes@gmail.com>
2019-06-24 09:17:17 +0200

    change par setting

--- R/methods.r
+++ R/methods.r
@@ -5,50 +5,50 @@
 
 print.hsa <- function(object){
   validate_hsa(object)
-  
+
   cat(paste("Number of origins                          :", length(unique(object$original_data$from)), "\n"))
   cat(paste("Number of destinations                     :", length(unique(object$original_data$to)), "\n"))
   cat(paste("Number of HSAs created                     :", length(object$names), "\n"))
   cat(paste("Number of iterations                       :", length(object$n_it), "\n"))
   cat(paste("Number of small HSAs (< min_interventions) :", sum(object$n_int < object$min_interventions), "\n"))
   cat(paste("Number of iterations                       :", length(object$n_it), "\n"))
-  
+
 }
 
 plot.hsa <- function(object, hsabcol = "black", regbcol = "grey", lakes = NULL, lakecol = "skyblue", ...){
   validate_hsa(object)
-  
+
   pars <- par("mai")
   on.exit(suppressWarnings(par(mai = pars)))
-  
+
   par(mai = c(0,0,0,0))
   plot(object$original_shp, border = regbcol)
-  
+
   if(!is.null(lakes)) plot(lakes, border = NA, col = lakecol, add = TRUE)
-  
+
   plot(object$shp, border = hsabcol, add = TRUE, ...)
-  
-  
+
+
 }
 
 plot.hsa.clust <- function(object, hsabcol = "black", regbcol = "grey", lakes = NULL, lakecol = "skyblue", ...){
-  
+
   pars <- par("mai")
   on.exit(suppressWarnings(par(mai = pars)))
-  
+
   par(mai = c(0,0,0,0))
   plot(object$original_shp, border = regbcol)
-  
+
   if(!is.null(lakes)) plot(lakes, border = NA, col = lakecol, add = TRUE)
-  
+
   plot(object$shp, border = hsabcol, add = TRUE, ...)
-  
-  
+
+
 }
 
 summary.hsa <- function(object, li = FALSE, n_interv = FALSE, plot = FALSE, ...){
   validate_hsa(object)
-  
+
   x <- object$li
   x <- data.frame(HSA = names(x), LI = unlist(x), stringsAsFactors = FALSE, row.names = NULL)
   x <- x[order(-x$LI),]
@@ -61,13 +61,13 @@ summary.hsa <- function(object, li = FALSE, n_interv = FALSE, plot = FALSE, ...)
   # } #else {
     # print(x, quote=FALSE, right=TRUE)
   # }
-  
+
   y <- as.data.frame(table(factor(object$reassigned_data$to_hsa, levels = unique(object$lookup$assignment))))
   names(y)[1] <- "HSA"
   y <- y[order(-y$Freq),]
-  
+
   method <- as.data.frame(table(object$method))
-  
+
   out <- list(# call = object$call,
               li_threshold = object$li_threshold,
               min_interv = object$min_interventions,
@@ -79,9 +79,9 @@ summary.hsa <- function(object, li = FALSE, n_interv = FALSE, plot = FALSE, ...)
               shp_src_to = object$original_data$to[!object$original_data$to %in% row.names(object$original_shp)],
               hsas = unique(object$shp@data$assignment),
               li = x,
-              n_interv = y, 
+              n_interv = y,
               assign = method)
-  
+
   # cat(paste("Call:\n", out$call, "\n"))
   cat("Criteria\n")
   cat(paste("   Localization Index (LI) threshold:   ", out$li_threshold,"\n"))
@@ -102,7 +102,7 @@ summary.hsa <- function(object, li = FALSE, n_interv = FALSE, plot = FALSE, ...)
   cat(out$hsas)
   cat("\nRegions assigned to HSAs via:\n")
   print(out$assign, quote=FALSE, right=TRUE, row.names=FALSE)
-  
+
   cat("\n\nLocalization Index\n")
   print(summary(as.numeric(out$li$LI)))
   if(li){
@@ -111,20 +111,20 @@ summary.hsa <- function(object, li = FALSE, n_interv = FALSE, plot = FALSE, ...)
   }
   cat("\nInterventions\n")
   print(summary(as.numeric(out$n_interv$Freq)))
-  
+
   if(n_interv) {
     cat("\n")
     print(out$n_interv, quote=FALSE, right=TRUE, row.names=FALSE)
   }
-  
+
   if(plot){
     if(class(object) == "hsa"){
       cat("\nCreating 3 plots\n")
       pars <- par("mai")
       on.exit(suppressWarnings(par(mai = pars)))
-      par(mai = c(1.02, 0.82, 0.42, 0.42))
+      # par(mai = c(1.02, 0.82, 0.42, 0.42))
       plot(object$n_it, xlab = "Iteration", ylab = "Number of (proto-)HSAs", las = 1, type = "b")
-      
+
       # interventions per HSA
       x <- table(factor(object$reassigned_data$to, levels = unique(object$lookup$assignment)))
       x <- x[order(x)]
@@ -132,21 +132,21 @@ summary.hsa <- function(object, li = FALSE, n_interv = FALSE, plot = FALSE, ...)
       text(x = 1:length(x), y = x, labels = x, srt = 90, pos = 3)
       abline(h = object$min_interventions, col = "red")
       legend("topleft", legend = "Threshold", col = "red", lty = 1, bty = "n")
-      
+
       # LI
       plot(object$li[order(object$li, na.last = FALSE)], xaxt = "n", ylab = "Localization index", ylim = c(min(min(object$li, na.rm = TRUE), object$li_threshold), max(object$li, na.rm = TRUE)))
       axis(1, at = 1:length(object$li), names(object$li)[order(object$li, na.last = FALSE)], las = 2)
       abline(h = object$li_threshold, col = "red")
       legend("topleft", legend = "Threshold", col = "red", lty = 1, bty = "n")
-    } 
+    }
     if(class(object) == "hsa.clust"){
       cat("\nCreating 1 plot\n")
       plot(object$scree)
     }
   }
-  
-  
-  
+
+
+
 }
 
 
@@ -163,15 +163,15 @@ summary.hsa <- function(object, li = FALSE, n_interv = FALSE, plot = FALSE, ...)
 # }
 
 validate_hsa <- function(object){
-  
+
   if(!is.data.frame(object$lookup))     stop("lookup slot should be data.frame")
   if(!is.list(object$lookups))          stop("lookups slot should be list")
   if(!is.list(object$original_data))    stop("original_data slot should be list")
   if(!is.list(object$reassigned_data))  stop("reassigned_data slot should be list")
-  if(!class(object$original_shp) == "SpatialPolygonsDataFrame"){     
+  if(!class(object$original_shp) == "SpatialPolygonsDataFrame"){
     stop("original_shp slot should be SpatialPolygonsDataFrame")
   }
-  if(!class(object$shp) == "SpatialPolygonsDataFrame"){ 
+  if(!class(object$shp) == "SpatialPolygonsDataFrame"){
     stop("original_shp slot should be SpatialPolygonsDataFrame")
   }
   if(!is.numeric(object$iterations))    stop("iterations slot should be numeric")
@@ -181,7 +181,7 @@ validate_hsa <- function(object){
   if(!is.numeric(object$n_it))          stop("n_it slot should be list")
   if(!is.character(object$names))       stop("names slot should be list")
   if(!is.data.frame(object$flow))       stop("flow slot should be list")
-  
+
 }
 
 
@@ -192,11 +192,11 @@ validate_hsa <- function(object){
 #   validate_hsa(object)
 #   object$shp
 # }
-# 
+#
 # as.SpatialPolygons <- function(object){
 #   UseMethod("as.SpatialPolygons", object)
 # }
 # as.SpatialPolygons.hsa <- function(object){
 #   validate_hsa(object)
-#   
+#
 # }
